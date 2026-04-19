31d9b693285515028c04dbc6d88b3d0fe8c1802d
markosykarlos <markosjimmm@gmail.com>
2026-04-20 00:49:43 +0200

    Proyecto

---
+++ PL-PPAA/src/main/java/com/mycompany/pl/ppaa/PLPPAA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pl.ppaa;
+
+/**
+ *
+ * @author marko
+ */
+public class PLPPAA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
