5f9b1edac4c76ad173ad578a38481ea6268f6b13
LUGIADH <lugia.sunshine@gmail.com>
2026-04-20 12:57:56 +0200

    un hilo que se llama generador de eventos y un monitor llamado estado global

--- ProyectoStrangerThings/src/main/java/hilos/generadoreventos.java
+++ ProyectoStrangerThings/src/main/java/hilos/generadoreventos.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hilos;
 
-/**
- *
- * @author LUGIA
- */
-public class generadoreventos {
-    
+import monitores.EstadoGlobal;
+import java.util.Random;
+
+public class generadoreventos extends Thread {
+    private EstadoGlobal estado;
+    private Random random;
+
+    public generadoreventos(EstadoGlobal estado) {
+        this.estado = estado;
+        this.random = new Random();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Generador de eventos globales activado.");
+
+        while (true) {
+            try {
+                
+                int tiempoEspera = 30000 + random.nextInt(30001);
+                Thread.sleep(tiempoEspera);
+
+              
+                int eventoElegido = 1 + random.nextInt(4);
+
+                // Activar el evento en el monitor
+                estado.setEventoActivo(eventoElegido);
+
+                String nombreEvento = "";
+                switch (eventoElegido) {
+                    case EstadoGlobal.APAGON_LABORATORIO:
+                        nombreEvento = "Apagón del Laboratorio";
+                        break;
+                    case EstadoGlobal.TORMENTA_UPSIDEDOWN:
+                        nombreEvento = "Tormenta del Upside Down";
+                        break;
+                    case EstadoGlobal.INTERVENCION_ELEVEN:
+                        nombreEvento = "Intervención de Eleven";
+                        break;
+                    case EstadoGlobal.RED_MENTAL:
+                        nombreEvento = "La Red Mental";
+                        break;
+                }
+                
+                System.out.println("!!! EVENTO GLOBAL INICIADO: " + nombreEvento + " !!!");
+
+                // Duración del evento: entre 5 y 10 segundos
+                int duracionEvento = 5000 + random.nextInt(5001);
+                Thread.sleep(duracionEvento);
+
+                //  Finalizar el evento volviendo a ponerlo a NINGUNO (0)
+                System.out.println("--- Evento " + nombreEvento + " finalizado. ---");
+                estado.setEventoActivo(EstadoGlobal.NINGUNO);
+
+            } catch (InterruptedException e) {
+                System.err.println("Generador de eventos interrumpido.");
+                break;
+            }
+        }
+    }
 }
