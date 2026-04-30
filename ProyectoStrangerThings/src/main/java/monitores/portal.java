2445123e30aa4016bf27d7ddb6c841ccc59fd949
LUGIADH <lugia.sunshine@gmail.com>
2026-04-30 17:21:10 +0200

    ahora portal bien, le pasaba inanicion

--- ProyectoStrangerThings/src/main/java/monitores/portal.java
+++ ProyectoStrangerThings/src/main/java/monitores/portal.java
@@ -1,48 +1,83 @@
-package monitores; 
-import hilos.*; 
-public class Portal { 
-    private int capacidad; 
-    private int esperandoIda = 0; 
-    private int esperandoVuelta = 0; 
-    private boolean ocupado = false; 
-    private EstadoGlobal estadoGlobal; 
-    private java.util.ArrayList<String> enTransito = new java.util.ArrayList<>(); 
-    public Portal(int capacidad, EstadoGlobal estadoGlobal) { 
-        this.capacidad = capacidad; 
-        this.estadoGlobal = estadoGlobal; } 
-    private int cruzando = 0; 
-    public synchronized void cruzarHaciaUpside(Nino n) { 
-        try { 
-            esperandoIda++; 
-            enTransito.add(n.getIdNino() + "(->)"); 
-            while (esperandoIda < capacidad || esperandoVuelta > 0 || estadoGlobal.getEventoActivo() == EstadoGlobal.APAGON_LABORATORIO) {
-                wait(); } 
-            if (esperandoIda == capacidad) { 
-                cruzando = capacidad; 
-                esperandoIda = 0; 
-                notifyAll(); } 
-            while (cruzando == 0 || ocupado) { 
-                wait(); } 
-            ocupado = true; 
-            cruzando--; 
-            System.out.println(n.getIdNino() + " cruzando hacia Upside Down"); 
+package monitores;
+
+import hilos.Nino;
+import java.util.ArrayList;
+
+public class Portal {
+    private int capacidad;
+    private int esperandoIda = 0;
+    private int cruzandoIda = 0; 
+    private int esperandoVuelta = 0;
+    private boolean ocupado = false;
+    private EstadoGlobal estadoGlobal;
+    private ArrayList<String> enTransito = new ArrayList<>();
+
+    public Portal(int capacidad, EstadoGlobal estadoGlobal) {
+        this.capacidad = capacidad;
+        this.estadoGlobal = estadoGlobal;
+    }
+
+    public synchronized void cruzarHaciaUpside(Nino n) {
+        try {
+            esperandoIda++;
+            enTransito.add(n.getIdNino() + "(->)");
+
+          
+            while (esperandoIda < capacidad || esperandoVuelta > 0 || cruzandoIda > 0 || estadoGlobal.getEventoActivo() == EstadoGlobal.APAGON_LABORATORIO) {
+                
+                
+                if (esperandoIda >= capacidad && esperandoVuelta == 0 && cruzandoIda == 0 && estadoGlobal.getEventoActivo() != EstadoGlobal.APAGON_LABORATORIO) {
+                    cruzandoIda = capacidad; 
+                    esperandoIda -= capacidad; 
+                    notifyAll(); 
+                    break;
+                }
+                wait();
+            }
+
+            
+            while (ocupado || estadoGlobal.getEventoActivo() == EstadoGlobal.APAGON_LABORATORIO || esperandoVuelta > 0) {
+                wait();
+            }
+
+            ocupado = true;
             Thread.sleep(1000); 
-            ocupado = false; 
-            enTransito.remove(n.getIdNino() + "(->)"); notifyAll(); } 
-        catch (InterruptedException e) {} } 
-    public synchronized void cruzarHaciaHawkins(Nino n) { 
-        try { 
-            esperandoVuelta++; 
-            enTransito.add(n.getIdNino() + "(<-)"); 
-            while (estadoGlobal.getEventoActivo() == EstadoGlobal.APAGON_LABORATORIO || ocupado){ 
-                wait();} 
-            ocupado = true; 
+            cruzandoIda--; 
+            ocupado = false;
+            
+            enTransito.remove(n.getIdNino() + "(->)");
+            notifyAll(); 
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public synchronized void cruzarHaciaHawkins(Nino n) {
+        try {
+            esperandoVuelta++;
+            enTransito.add(n.getIdNino() + "(<-)");
+
+            
+            while (ocupado || estadoGlobal.getEventoActivo() == EstadoGlobal.APAGON_LABORATORIO) {
+                wait();
+            }
+
+            ocupado = true;
             esperandoVuelta--; 
-            System.out.println(n.getIdNino() + " volviendo a Hawkins"); Thread.sleep(1000); 
-            ocupado = false; 
-            enTransito.remove(n.getIdNino() + "(<-)"); notifyAll(); } 
-        catch (InterruptedException e) {} } 
-    public synchronized String getIDs() { 
-        return String.join(", ", enTransito); 
-    } 
+            Thread.sleep(1000);
+            ocupado = false;
+            
+            enTransito.remove(n.getIdNino() + "(<-)");
+            notifyAll();
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public synchronized String getIDs() {
+        if (enTransito.isEmpty()) return "";
+        return String.join(", ", enTransito);
+    }
 }
